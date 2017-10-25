01158157c3da3fc04402675f3d46679a6498002d
Temo Kiknavelidze <temo.kiknavelidze@gmail.com>
2017-10-26 02:35:05 +0400

    17 BT Using virtual and override
    Added check to print the tank name if player is controlling any or message otherwise. (using UE_LOG inside TankPlayerController.cpp)

--- Source/BattleTank/TankPlayerController.cpp
+++ Source/BattleTank/TankPlayerController.cpp
@@ -3,6 +3,25 @@
 #include "TankPlayerController.h"
 
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("PlayerController Begin Play"));
+
+	// Pointer on the tank controlled by tank player
+	auto ControlledTank = GetControlledTank();
+	/// if player controlles a tank print name, otherwise print message
+	if (ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController possesing: %s"), *ControlledTank->GetName());
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController Not possesing a tank"));
+	}
+}
+
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
