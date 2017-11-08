19b9751dbb1ecc643145caa82d94bf7ec7f135e3
Temo Kiknavelidze <temo.kiknavelidze@gmail.com>
2017-11-08 23:27:18 +0400

    46 BT Using BlueprintReadOnly
    logging left thumbstick y value

--- Source/BattleTank/TankPlayerController.cpp
+++ Source/BattleTank/TankPlayerController.cpp
@@ -6,19 +6,6 @@
 void ATankPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
-	UE_LOG(LogTemp, Warning, TEXT("PlayerController Begin Play"));
-
-	// Pointer on the tank controlled by tank player
-	auto ControlledTank = GetControlledTank();
-	/// if player controls the tank print name, otherwise print message
-	if (ControlledTank)
-	{
-		UE_LOG(LogTemp, Warning, TEXT("PlayerController possesing: %s"), *ControlledTank->GetName());
-	}
-	else
-	{
-		UE_LOG(LogTemp, Warning, TEXT("PlayerController Not possesing a tank"));
-	}
 }
 
 // Called every frame
