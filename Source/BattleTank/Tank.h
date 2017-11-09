b660f6bf1eb4a7ab3769b3d1659187f4d52db889
Temo Kiknavelidze <temo.kiknavelidze@gmail.com>
2017-11-09 20:02:26 +0400

    47 BT A Better Component Architecture
    change: now tank moves with gamepad left thumbstick

--- Source/BattleTank/Tank.h
+++ Source/BattleTank/Tank.h
@@ -36,9 +36,6 @@ protected:
 	virtual void BeginPlay() override;
 
 	UTankAimingComponent* TankAimingComponent = nullptr;
-
-	UPROPERTY(BlueprintReadOnly)
-	UTankMovementComponent* TankMovementComponent = nullptr;
 	
 private:
 	ATank();
