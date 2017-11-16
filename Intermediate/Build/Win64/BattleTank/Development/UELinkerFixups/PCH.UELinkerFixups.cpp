cbac5ee41a6f6819dacd8981c7190b1bc1a3e264
Temo Kiknavelidze <temo.kiknavelidze@gmail.com>
2017-11-16 20:17:04 +0400

    55 BT Project version update

--- Intermediate/Build/Win64/BattleTank/Development/UELinkerFixups/PCH.UELinkerFixups.cpp
+++
@@ -1,1 +0,0 @@
-#include "C:/Users/timro_000/source/repos/BattleTank/Intermediate/Build/Win64/BattleTank/Development/UELinkerFixups/PCH.UELinkerFixups.h"
--- Intermediate/Build/Win64/BattleTank/Development/UELinkerFixups/PCH.UELinkerFixups.h
+++
@@ -1,2 +0,0 @@
-// PCH for C:\Users\timro_000\source\repos\BattleTank\Intermediate\Build\Win64\UE4\Development\UELinkerFixupsName.h
-#include "C:\Users\timro_000\source\repos\BattleTank\Intermediate\Build\Win64\UE4\Development\UELinkerFixupsName.h"
