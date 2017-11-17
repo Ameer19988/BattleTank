ff0a99b70a795bf3d0bc53bf9f66319ed51888c7
Temo Kiknavelidze <temo.kiknavelidze@gmail.com>
2017-11-17 14:55:50 +0400

    57 BT Using Enum(erations) in UE4
    using enum to set aiming color.

--- Source/BattleTank/TankAimingComponent.h
+++ Source/BattleTank/TankAimingComponent.h
@@ -8,6 +8,12 @@
 #include "Components/ActorComponent.h"
 #include "TankAimingComponent.generated.h"
 
+
+// Enum for aiming state
+UENUM()
+enum class EFiringState : uint8 
+{ Locked, Aiming, Reloading };
+
 // forward declaration
 class UTankBarrel; 
 class UTankTurret;
@@ -23,10 +29,12 @@ public:
 	UTankAimingComponent();
 	void SetBarrelReference(UTankBarrel* BarrelToSet);
 	void SetTurretReference(UTankTurret* TurretToSet);
-
-public:	
 	void AimAt(FVector HitLocation, float LaunchSpeed);
 
+protected:	
+	UPROPERTY(BlueprintReadOnly, Category = "State")
+	EFiringState FiringState = EFiringState::Reloading;
+	
 private:
 	UTankBarrel* Barrel = nullptr;
 	UTankTurret* Turret = nullptr;
